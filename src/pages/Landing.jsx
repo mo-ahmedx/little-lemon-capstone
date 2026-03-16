aafd6067241f328b9a4c4b7d580c06ac7d4473d8
mo-ahmedx <mohammedahmedk886@gmail.com>
2026-03-16 22:24:57 +0300

    feat: Add BookingForm and BookingPage | Fix routes for Booking Order online and Login pages

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -4,6 +4,7 @@ import Special from '../components/Special.jsx';
 import Testimonial from "../components/Testimonial.jsx";
 import About from "../components/About.jsx";
 import Footer from '../components/Footer.jsx';
+import BookingPage from "./BookingPage.jsx";
 export default function Landing(){
     return (
         <>
