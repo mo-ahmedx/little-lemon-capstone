46745da5724f22ad75c958e709f0791c6a249862
mo-ahmedx <mohammedahmedk886@gmail.com>
2026-03-30 08:03:20 +0300

    feat:implement useReducer for booking logic and setup Vitest suite

--- src/pages/BookingPage.jsx
+++ src/pages/BookingPage.jsx
@@ -1,8 +1,39 @@
-import BookingForm from "../components/BookingForm.jsx"
-export default function BookingPage(){
-    return(
-        <>
-        <BookingForm />
-        </>
-    )
+import { useState, useReducer } from "react";
+import BookingForm from "../components/BookingForm.jsx";
+export default function BookingPage() {
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
+  const [guests, setGuests] = useState(1);
+  const [occasion, setOccasion] = useState("");
+
+  const initialTimes = ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
+
+  const updateTime = (state, action ) => {
+        switch(action.type){
+            case "UPDATE_TIMES":
+                return initialTimes;
+            default:
+                return state;    
+        }
+  };
+
+  const [availableTimes, dispatch] = useReducer(updateTime, initialTimes);
+
+  return (
+    <>
+      <BookingForm 
+        date={date}
+        setDate={setDate}
+        time={time}
+        setTime={setTime}
+        occasion={occasion}
+        setOccasion={setOccasion}
+        guests={guests}
+        setGuests={setGuests}
+        availableTimes={availableTimes}
+        dispatch={dispatch}
+      
+      />
+    </>
+  );
 }
